f0640062c1d980061fd4092c58109f807c1c0d6f
Braxton Lowe <56900096+CobaltRebellion@users.noreply.github.com>
2024-12-08 23:25:00 -0800

    Add files via upload

--- gis.rb
+++ gis.rb
@@ -1,53 +1,20 @@
 #!/usr/bin/env ruby
 
 class Track
+
+  attr_reader :name, :segments
+
   def initialize(segments, name=nil)
     @name = name
     segment_objects = []
-    segments.each do |s|
-      segment_objects.append(TrackSegment.new(s))
+    segments.each do |segment|
+      segment_objects.append(TrackSegment.new(segment))
     end
     # set segments to segment_objects
     @segments = segment_objects
   end
-
-  def get_track_json()
-    j = '{'
-    j += '"type": "Feature", '
-    if @name != nil
-      j+= '"properties": {'
-      j += '"title": "' + @name + '"'
-      j += '},'
-    end
-    j += '"geometry": {'
-    j += '"type": "MultiLineString",'
-    j +='"coordinates": ['
-    # Loop through all the segment objects
-    @segments.each_with_index do |s, index|
-      if index > 0
-        j += ","
-      end
-      j += '['
-      # Loop through all the coordinates in the segment
-      tsj = ''
-      s.coordinates.each do |c|
-        if tsj != ''
-          tsj += ','
-        end
-        # Add the coordinate
-        tsj += '['
-        tsj += "#{c.lon},#{c.lat}"
-        if c.ele != nil
-          tsj += ",#{c.ele}"
-        end
-        tsj += ']'
-      end
-      j+=tsj
-      j+=']'
-    end
-    j + ']}}'
-  end
 end
+
 class TrackSegment
   attr_reader :coordinates
   def initialize(coordinates)
@@ -56,9 +23,7 @@ class TrackSegment
 end
 
 class Point
-
   attr_reader :lat, :lon, :ele
-
   def initialize(lon, lat, ele=nil)
     @lon = lon
     @lat = lat
@@ -66,93 +31,165 @@ class Point
   end
 end
 
-class Waypoint
+class Waypoint  
+  attr_reader :lat, :lon, :ele, :name, :icon
 
-
-
-attr_reader :lat, :lon, :ele, :name, :type
-
-  def initialize(lon, lat, ele=nil, name=nil, type=nil)
+  def initialize(lon, lat, ele=nil, name=nil, icon=nil)
     @lat = lat
     @lon = lon
     @ele = ele
     @name = name
-    @type = type
+    @icon = icon
   end
 
-  def get_waypoint_json(indent=0)
-    j = '{"type": "Feature",'
-    # if name is not nil or type is not nil
-    j += '"geometry": {"type": "Point","coordinates": '
-    j += "[#{@lon},#{@lat}"
-    if ele != nil
-      j += ",#{@ele}"
-    end
-    j += ']},'
-    if name != nil or type != nil
-      j += '"properties": {'
-      if name != nil
-        j += '"title": "' + @name + '"'
-      end
-      if type != nil  # if type is not nil
-        if name != nil
-          j += ','
-        end
-        j += '"icon": "' + @type + '"'  # type is the icon
-      end
-      j += '}'
-    end
-    j += "}"
-    return j
-  end
 end
 
 class World
-def initialize(name, things)
-  @name = name
-  @features = things
-end
-  def add_feature(f)
-    @features.append(t)
+  def initialize(name, things)
+    @name = name
+    @features = things
   end
 
+  def add_feature(new_feature)
+    @features.append(new_feature)
+  end
+
+  # Formats all
   def to_geojson(indent=0)
-    # Write stuff
-    s = '{"type": "FeatureCollection","features": ['
-    @features.each_with_index do |f,i|
+    json = '{"type": "FeatureCollection","features": ['
+    @features.each_with_index do |feature, i|
+
       if i != 0
-        s +=","
+        json += ","
       end
-        if f.class == Track
-            s += f.get_track_json
-        elsif f.class == Waypoint
-            s += f.get_waypoint_json
+
+      json += feature
+
+    end
+    json + "]}"
+  end
+  
+end
+
+# ------------------------------------------------------------------------------------------------
+class Formatting
+
+  attr_reader :lat, :lon, :ele, :name, :icon
+
+  def initialize(name=nil, lat=nil, lon=nil, ele=nil, icon=nil, segments=nil)
+    @lat = lat
+    @lon = lon
+    @ele = ele
+    @name = name
+    @icon = icon
+    @segments = segments
+  end
+
+  def get_properties_json()
+    if name != nil or icon != nil
+      json = '"properties": {'
+      if name != nil
+        json += '"title": "' + @name + '"'
+      end
+
+      # if its a waypoint, and has an icon
+      if icon != nil  # if icon is not nil
+        if name != nil
+          json += ','
+        end
+        json += '"icon": "' + @icon + '"'
+      end
+      json += '}'
+    end
+  end
+
+  def get_geometry_json()
+    json = '"geometry": {'
+
+    # if its a waypoint, do this
+    if lon != nil
+      json += '"type": "Point","coordinates": '
+      json += "[#{@lon},#{@lat}"
+      if ele != nil
+        json += ",#{@ele}"
+      end
+
+    # if its a track, do this
+    elsif @segments != nil
+      json += '"type": "MultiLineString",'
+      json +='"coordinates": ['
+
+      @segments.each_with_index do |segment, index|
+        if index > 0
+          json += ","
+        end
+        json += '['
+        
+        # Loop through all the coordinates in the segment
+        cordjson = ''
+        segment.coordinates.each do |coordinate|
+          if cordjson != ''
+            cordjson += ','
+          end
+
+          # Add the coordinate
+          cordjson += '['
+          cordjson += "#{coordinate.lon},#{coordinate.lat}"
+          
+          if coordinate.ele != nil
+            cordjson += ",#{coordinate.ele}"
+          end
+          cordjson += ']'
+        end
+        json += cordjson
+        json += ']'
       end
     end
-    s + "]}"
+    json += ']}}'
   end
+
+  def get_json(indent=0)
+    json = '{"type": "Feature",'
+
+    properties = self.get_properties_json
+    json += properties + ","
+    
+    geometry = self.get_geometry_json
+    json += geometry
+  end
+
 end
 
 def main()
   w = Waypoint.new(-121.5, 45.5, 30, "home", "flag")
   w2 = Waypoint.new(-121.5, 45.6, nil, "store", "dot")
+  
   ts1 = [
   Point.new(-122, 45),
   Point.new(-122, 46),
   Point.new(-121, 46),
   ]
 
-  ts2 = [ Point.new(-121, 45), Point.new(-121, 46), ]
+  ts2 = [ 
+  Point.new(-121, 45), 
+  Point.new(-121, 46), 
+  ]
 
   ts3 = [
-    Point.new(-121, 45.5),
-    Point.new(-122, 45.5),
+  Point.new(-121, 45.5),
+  Point.new(-122, 45.5),
   ]
 
   t = Track.new([ts1, ts2], "track 1")
   t2 = Track.new([ts3], "track 2")
 
-  world = World.new("My Data", [w, w2, t, t2])
+  f1 = Formatting.new(w.name, w.lat, w.lon, w.ele, w.icon)
+  f2 = Formatting.new(w2.name, w2.lat, w2.lon, w2.ele, w2.icon)
+  f3 = Formatting.new(t.name, nil, nil, nil, nil, t.segments)
+  f4 = Formatting.new(t2.name, nil, nil, nil, nil, t2.segments)
+
+
+  world = World.new("My Data", [f1.get_json, f2.get_json, f3.get_json, f4.get_json])
 
   puts world.to_geojson()
 end
--- tc_gis.rb
+++ tc_gis.rb
@@ -7,17 +7,20 @@ class TestGis < Test::Unit::TestCase
   def test_waypoints
     w = Waypoint.new(-121.5, 45.5, 30, "home", "flag")
     expected = JSON.parse('{"type": "Feature","properties": {"title": "home","icon": "flag"},"geometry": {"type": "Point","coordinates": [-121.5,45.5,30]}}')
-    result = JSON.parse(w.get_waypoint_json)
+    f1 = Formatting.new(w.name, w.lat, w.lon, w.ele, w.icon)
+    result = JSON.parse(f1.get_json)
     assert_equal(result, expected)
 
     w = Waypoint.new(-121.5, 45.5, nil, nil, "flag")
     expected = JSON.parse('{"type": "Feature","properties": {"icon": "flag"},"geometry": {"type": "Point","coordinates": [-121.5,45.5]}}')
-    result = JSON.parse(w.get_waypoint_json)
+    f1 = Formatting.new(w.name, w.lat, w.lon, w.ele, w.icon)
+    result = JSON.parse(f1.get_json)
     assert_equal(result, expected)
 
     w = Waypoint.new(-121.5, 45.5, nil, "store", nil)
     expected = JSON.parse('{"type": "Feature","properties": {"title": "store"},"geometry": {"type": "Point","coordinates": [-121.5,45.5]}}')
-    result = JSON.parse(w.get_waypoint_json)
+    f1 = Formatting.new(w.name, w.lat, w.lon, w.ele, w.icon)
+    result = JSON.parse(f1.get_json)
     assert_equal(result, expected)
   end
 
@@ -37,12 +40,14 @@ class TestGis < Test::Unit::TestCase
 
     t = Track.new([ts1, ts2], "track 1")
     expected = JSON.parse('{"type": "Feature", "properties": {"title": "track 1"},"geometry": {"type": "MultiLineString","coordinates": [[[-122,45],[-122,46],[-121,46]],[[-121,45],[-121,46]]]}}')
-    result = JSON.parse(t.get_track_json)
+    f1 = Formatting.new(t.name, nil, nil, nil, nil, t.segments)
+    result = JSON.parse(f1.get_json)
     assert_equal(expected, result)
 
     t = Track.new([ts3], "track 2")
     expected = JSON.parse('{"type": "Feature", "properties": {"title": "track 2"},"geometry": {"type": "MultiLineString","coordinates": [[[-121,45.5],[-122,45.5]]]}}')
-    result = JSON.parse(t.get_track_json)
+    f1 = Formatting.new(t.name, nil, nil, nil, nil, t.segments)
+    result = JSON.parse(f1.get_json)
     assert_equal(expected, result)
   end
 
@@ -65,7 +70,12 @@ class TestGis < Test::Unit::TestCase
     t = Track.new([ts1, ts2], "track 1")
     t2 = Track.new([ts3], "track 2")
 
-    w = World.new("My Data", [w, w2, t, t2])
+    f1 = Formatting.new(w.name, w.lat, w.lon, w.ele, w.icon)
+    f2 = Formatting.new(w2.name, w2.lat, w2.lon, w2.ele, w2.icon)
+    f3 = Formatting.new(t.name, nil, nil, nil, nil, t.segments)
+    f4 = Formatting.new(t2.name, nil, nil, nil, nil, t2.segments)
+
+    w = World.new("My Data", [f1.get_json, f2.get_json, f3.get_json, f4.get_json])
 
     expected = JSON.parse('{"type": "FeatureCollection","features": [{"type": "Feature","properties": {"title": "home","icon": "flag"},"geometry": {"type": "Point","coordinates": [-121.5,45.5,30]}},{"type": "Feature","properties": {"title": "store","icon": "dot"},"geometry": {"type": "Point","coordinates": [-121.5,45.6]}},{"type": "Feature", "properties": {"title": "track 1"},"geometry": {"type": "MultiLineString","coordinates": [[[-122,45],[-122,46],[-121,46]],[[-121,45],[-121,46]]]}},{"type": "Feature", "properties": {"title": "track 2"},"geometry": {"type": "MultiLineString","coordinates": [[[-121,45.5],[-122,45.5]]]}}]}')
     result = JSON.parse(w.to_geojson)
